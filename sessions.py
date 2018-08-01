ca3a644c4e91d566c1d99b6c2de304091b4cccf6
Aaron Gruberg <agruberg@DB-MBP-22G8WP.local>
2018-08-01 16:01:25 -0700

    First attempt at creating sessions column to show number of pageviews per user within a 30 minute time frame.

---
+++ sessions.py
@@ -0,0 +1,142 @@
+# Get the number of sessions and page views per session where a session is the total page views 
+# for a given SID over 30 minute period from first page view
+
+import pandas as pd
+import sys  
+from datetime import datetime
+import numpy as np
+
+# Changed default string encoding from 'ascii' to 'utf8'
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+### READ CSV 
+
+f = 'ftse.0510-0622.2018.api.logs_SAMPLE.csv'
+
+df = pd.read_csv(f, skiprows=None)
+df = df.head(10)
+
+# Convert to datetime format
+df['date_time'] =  pd.to_datetime(df['date_time'])
+df['date_time'] = df['date_time'].values
+#print type(df['date_time'])
+#print df
+
+
+#---------------------------------------------------------------------------------------------
+
+### Find sessions for one SID
+
+# First find the time between two pageviews for a single SID
+# Test SID: '9643371'
+def sessions(sid, datetime, session_time):
+
+	times = []
+	sessions = []
+	#datetime = datetime.to_pydatetime()
+	#print type(datetime)
+
+	# SID is not in pageviews dictionary
+	if sid not in pageviews.keys():
+		#print "first"
+		
+		pageviews[sid] = [datetime]
+		#print pageviews
+
+	# SID only has 1 list of time values, the last element is a numpy object.
+	# Compare datetime to the first element.
+
+	#elif isinstance(pageviews[sid][-1], np.datetime64):
+	elif len(pageviews[sid]) == 1:
+
+		#print "second"
+
+		initial_time = pageviews[sid][0]
+
+		diff = datetime - initial_time
+		diff = diff.seconds
+		#print diff
+
+		# Convert time difference to minutes
+		#diff = diff.astype('timedelta64[m]')
+		#diff = diff.astype(str)
+		#diff = diff.split(' ')
+		#diff = diff[0]
+		#diff = int(diff)
+
+		if diff <= session_time:
+
+			pageviews[sid].append(datetime)
+
+		else:
+
+			pageviews[sid] = [pageviews[sid]]
+			pageviews[sid].append([datetime])
+
+	# SID time values are stored in a list of lists.
+	# Compare datetime to the first element of the last list.
+	else:
+		#print "third"
+		initial_time = pageviews[sid][-1][0]
+
+		diff = datetime - initial_time
+		diff = diff.seconds
+
+		# Convert time difference to minutes
+		#diff = diff.astype('timedelta64[m]')
+		#diff = diff.astype(str)
+		#diff = diff.split(' ')
+		#diff = diff[0]
+		#diff = int(diff)
+
+		if diff <= session_time:
+
+			pageviews[sid][-1].append(datetime)
+
+		else:
+
+			pageviews[sid].append([datetime])
+
+	session_count = len(pageviews[sid])
+
+	return session_count
+
+#-----------------------------------------------------------------------------------------------
+
+### TEST VALUES
+
+#test = df[df['demandbase_sid'] == 9643371]
+#dt1 = test['date_time'].values[0]
+#dt2 = test['date_time'].values[1]
+#dt3 = test['date_time'].values[2]
+
+# Store SID and datetime in pageviews dict
+#pageviews = {9643371: [[dt1, dt2], [dt2, dt2]]}
+#pageviews = {9643371: [[dt1, dt2]]}
+#pageviews = {9643371: [dt1, dt2]}
+#pageviews = {}
+#session_time = 30
+
+#print sessions(9643371, dt3, session_time)
+
+#-----------------------------------------------------------------------------------------------
+
+### APPLY sessions() TO EVERY ROW
+
+pageviews = {}
+
+# This is the number of seconds in 30 minutes
+session_time = 1800
+
+#print df['date_time'].values
+
+df['sessions'] = df.apply(lambda x: sessions(x['demandbase_sid'], x['date_time'], session_time), axis=1)
+
+print df
+
+print pageviews
+
+
+
+
